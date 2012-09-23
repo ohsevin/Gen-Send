4d623fec795d22e3cec077dad99552054a395fa9
Matt Brunt <openblue555@gmail.com>
2012-09-23 14:25:29 +0100

    Function name changes.

--- system/init.php
+++ system/init.php
@@ -5,7 +5,7 @@ require_once (ROOT . DS . 'system' . DS . 'shared_functions.php');
 
 gzip_output() || ob_start("ob_gzhandler");
 
-set_reporting(); // set error reporting levels here
+set_error_reporting(); // set error reporting levels here
 
 $cache = Cache::get_instance();
 $inflect = Inflection::get_instance();
@@ -16,4 +16,4 @@ remove_magic_quotes();
 unregister_globals();
 
 // let's go!
-run_this_show($url, $default, $autoload, $extra_query_string, $routing);
+run_calls($url, $default, $autoload, $extra_query_string, $routing);
