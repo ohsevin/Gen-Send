5178b82a2b65445d89dacc03c0f3ba56396e1600
Matt Brunt <openblue555@gmail.com>
2012-09-23 21:54:15 +0100

    404 youtube video removal.

--- application/views/fourohfour/index.php
+++ application/views/fourohfour/index.php
@@ -1,7 +1,5 @@
 <div id="maffyoo">
     <div id="headlines">
         <h3>I'm sorry, Dave, I'm afraid I can't do that.</h3>
-        <iframe width="400" height="300" src="http://www.youtube.com/embed/7qnd-hdmgfk" frameborder="0" allowfullscreen></iframe>
-        
     </div>
 </div>
