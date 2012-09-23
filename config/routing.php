0d640cfc94795d232a086419f4c65da2f51bd48c
Matt Brunt <openblue555@gmail.com>
2012-09-23 03:07:51 +0100

    Routing update
    site_url/v/hfuirudh actually acts as site_url/securesend/v/hfuirudh
    and site_url/securesend/v/hfuirudh still works as normal

--- config/routing.php
+++ config/routing.php
@@ -1,7 +1,10 @@
 <?php
 
+// order is important in this array
 $routing = array(
-	'/admin\/(.*?)\/(.*?)\/(.*)/' => 'admin/\1_\2/\3'
+	'/admin\/(.*?)\/(.*?)\/(.*)/'      =>      'admin/\1_\2/\3',
+    '/securesend\/v\/(.*)/'            =>      'securesend/v/\1', // go from securesend/v/$url to securesend/v/$url
+    '/v\/(.*)/'                        =>      'securesend/v/\1' // go from /v/$url to /securesend/v/$url
 );
 
 $default['controller'] = 'siteindex';
