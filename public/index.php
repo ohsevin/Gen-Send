396b45729c8454b0c03056a7ec4870443e83451b
Matt Brunt <openblue555@gmail.com>
2012-10-09 23:43:01 +0100

    Fix for error messages being thrown on index.php for no 'url' index in the $_GET array after re-writes.

--- public/index.php
+++ public/index.php
@@ -3,9 +3,14 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
-$url = $_GET['url']; // get our URL (rewritten by .htaccess (or equivalent) re-writes)
+$url = null; // set URL to null by default.
 
-unset($_GET['url']); // unset the URL from $_GET array
+if(isset($_GET['url'])) // if we have a URL in our $_GET array
+{
+    $url = $_GET['url']; // get our URL (rewritten by .htaccess (or equivalent) re-writes)
+    unset($_GET['url']); // unset the URL from $_GET array 
+}
+
 
 // load bootstrap
 require_once(ROOT . DS . 'system' . DS . 'bootstrap.php');
