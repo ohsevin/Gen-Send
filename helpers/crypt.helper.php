ebdd6c5e3ad317b415884c5b29db4477bd266426
Matt Brunt <openblue555@gmail.com>
2013-02-02 16:12:30 +0000

    Change to random number function in crypt.

--- helpers/crypt.helper.php
+++ helpers/crypt.helper.php
@@ -1,17 +1,19 @@
 <?php
+
+
 /**
 * Cryptographically secure helper
 *
-* @package    Utils
+* @package    Gensend
 * @author     Matt Brunt (http://maffyoo.co.uk)
-* @version    0.0.2
+* @version    0.0.3
 */
 
-class GNSND_Crypt {
-	
-	/**
+class Crypt {
+    
+    /**
      *    Generates a random number between two given values
-	 *    More secure than rand() 
+     *    More secure than rand() 
      *
      *    @param    int        Minimum number
      *    @param    int        Maximum number
@@ -19,16 +21,24 @@ class GNSND_Crypt {
      *
      *    @access    public
      */
-    public function random_number($min = 0, $max = 1000000)
-    {
+    public function random_number($min = 0, $max = 1000000) {
         // get our range
         $range = $max - $min;
         
-        if ($range == 0) return $min; // return because there's no difference, oops.
+        if ($range == 0) return $min; // uh-oh.
         
-        // get the length of bytes we should generate
-        $length = (int)(log($range, 2) / 8) + 1;
-		
-        return $min + (hexdec(bin2hex(openssl_random_pseudo_bytes($length, $s))) % $range);
+        $log = log($range, 2);
+        $bytes = (int) ($log / 8) + 1; // length in bytes
+        $bits = (int) $log + 1; // length in bits
+        
+        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+        
+        do
+        {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes, $s)));
+            $rnd = $rnd & $filter; // discard irrelevant bits
+        } while ($rnd >= $range);
+        
+        return $min + $rnd;
     }
 }
